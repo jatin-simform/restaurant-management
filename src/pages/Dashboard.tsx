3ec6f9fded7ea352d47fea45dc0fde783a6232da
Jatin Parmar <jatin.parmar@simformsolutions.com>
2025-01-13 19:40:13 +0530

    worked on dashboard design

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,23 +2,28 @@ import { Grid2, Paper } from "@mui/material"
 import React from "react"
 import Indicator, { IIndicatorProps } from "../components/Dashboard/Indicator"
 import WelcomeBoard from "../components/Dashboard/WelcomBoard"
+import useCategories from "../hooks/useCategories"
+import useRecipes from "../hooks/useRecipes"
+import useMenu from "../hooks/useMenu"
 
-interface IDashboardProps {
-   indicators: IIndicatorProps[],
-}
 
-const Dashboard: React.FC<IDashboardProps> = ({ indicators }) => {
+const Dashboard: React.FC = () => {
+   const { items: categories } = useCategories();
+   const { items: recipes } = useRecipes();
+   const { items: menus } = useMenu();
+
+
 
    return <>
       <Paper elevation={24} style={{ padding: "5px", margin: "0px auto", marginTop: 25, width: '90%', height: "80vh" }}>
          <WelcomeBoard />
-         {
-            indicators.length > 0 && <Grid2 container spacing={4} padding={10} justifyContent={'center'} alignItems={'center'} >
-               {indicators.map((t, index) => <Indicator key={index} {...t} />)}
-            </Grid2>
-         }
-      </Paper>
+         <Grid2 container spacing={4} padding={10} justifyContent={'center'} alignItems={'center'} >
+            <Indicator count={menus.length} text="Menus" />
+            <Indicator count={categories.length} text="Categories" />
+            <Indicator count={recipes.length} text="Recipes" />
+         </Grid2>
 
+      </Paper>
    </>
 
 }
